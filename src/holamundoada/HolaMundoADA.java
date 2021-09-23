6e911da5b73b6bdcc08045cf9116b748a7e7832c
akaron79 <vicente.angel.lopez@gmail.com>
2021-09-24 00:02:29 +0200

    Boceto de iFrame para juego buscafrases

--- src/holamundoada/HolaMundoADA.java
+++ src/holamundoada/HolaMundoADA.java
@@ -5,6 +5,19 @@
  */
 package holamundoada;
 
+//Array de Frases leído de fichero
+//Se pregunta qué se quiere hacer (jugar o cargar partida)
+//Si es jugar se elige una frase y empieza el loop de juego
+//Si es cargar, se lee de fichero todo lo que se ha guardado y continúa el loop del juego
+//En el loop de juego se muestran con asteriscos lo que no se conoce y las letras que se conocen
+//Se cuenta el número de intentos
+//Las opciones en el bucle son: 
+// . Descubrir letra ==> cuenta un intento y descubre los aciertos
+// . Resolver ==> pide la frase entera: si coincide acaba y da puntos. Si no, quita puntos
+// . Guardar ==> almacena en disco el estado del juego y continúa pidiendo intentos.
+
+//Clases: Partida, Frase
+
 /**
  *
  * @author vicente
@@ -18,5 +31,5 @@ public class HolaMundoADA {
         // TODO code application logic here
         System.out.println("Hola mundo ADA");
     }
-    
+ 
 }
